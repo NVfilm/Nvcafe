880a268b22d328264012fdb9e046e16a01e9ce62
NVfilm <dadadesigner19@gmail.com>
2026-05-08 11:19:23 +0530

    Remove form listener and log website load
    Remove form submission handling and add console log.

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const form = document.getElementById("leadForm");
-
-form.addEventListener("submit", async (e) => {
-
-    e.preventDefault();
-
-    alert("Thank You! Our team will contact you shortly.");
-
-    form.reset();
-
-});
+console.log("NV Cafe Website Loaded");
